78b60e29d1e20d9b9cd3ef1595bb30d6d07c1bce
John Ortiz Ordoñez <johnortizo@outlook.com>
2015-12-11 11:22:04 -0500

    Popular Movies project creation

---
+++ app/src/main/java/ortizol/co/popularmovies/MainActivity.java
@@ -0,0 +1,19 @@
+package ortizol.co.popularmovies;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MoviesFragment())
+                    .commit();
+        }
+    }
+}
